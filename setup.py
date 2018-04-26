c8147cad6207fccb9c4f367eacc2772e548fea05
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2018-04-26 13:57:28 -0400

    upgrade

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='boot.py',
     author='Mario César Señoranis Ayala',
     author_email='mariocesar.c50@gmail.com',
-    version='0.14',
+    version='0.15',
     url='https://github.com/mariocesar/boot.py',
     description=description,
     long_description=f'\n{long_description}',
